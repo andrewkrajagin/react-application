90e287e0c24cc6266f09d4512c2d760d351cd456
Andrew Karjagin <42044197+andrewkrajagin@users.noreply.github.com>
2018-08-04 23:57:17 +0300

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,25 +1,6 @@
 import React from 'react';
 
 export default function Button(props) {
-  /* state = {
-    clickCounter: 0,
-    currentTimestamp: new Date(),
-  };
-
-  handleClick = () => {
-    this.setState((prevState) => {
-     return { clickCounter: prevState.clickCounter + 1 };
-    });
-  };
-
-  componentDidMount() {
-   setInterval(() => {
-     this.setState({ currentTimestamp: new Date() })
-    }, 1000);
-  } */
-  /* onClick = (e) => {
-
-  }; */
   return (
     <button
       type="button"
